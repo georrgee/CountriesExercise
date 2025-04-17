8765eedef9b760417316864a58079d86f88352ef
George Garcia <garcia.george.b@gmail.com>
2025-04-17 11:38:29 -0700

    Initial Commit

---
+++ CountriesExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesExercise
+//
+//  Created by George Garcia on 4/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
